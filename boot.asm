7e9842bab94fb20bdc33a6b126be0c6ee83faf5a
Takamatsu Shirako <137693283+Shirakowo@users.noreply.github.com>
2026-03-31 03:14:31 +0800

    Initial commit

---
+++ boot.asm
@@ -0,0 +1,26 @@
+[org 0x7c00]
+[bits 16]
+
+start:
+    mov si, msg
+    call print_str
+
+    jmp $
+
+print_str:
+    mov ah, 0x0e
+
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .loop
+
+.done:
+    ret
+
+msg: db 'Hello OS :3', 0
+
+times 510 - ($ - $$) db 0
+dw 0xaa55
